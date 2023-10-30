b191da1beae2fbc8de971d57493a4585723972f8
qeed97 <qeed97@gmail.com>
2023-10-30 12:11:04 +0100

    fixed alphabet div and added checclickedinsecret

--- script.js
+++ script.js
@@ -2,16 +2,24 @@ let abc = "abcdefghijklmnopqrstuvwxyz";
 
 let secretword = "codecool";
 
-function displayByLetter (word, classOf){
+function displayByLetter (word, classOf, clicked = ''){
 
     word = word.split('');
     
     const divTarget = document.querySelector(`.${classOf}`);
 
+    divTarget.innerHTML = '';
+
     word.forEach(letter => {
-        divTarget.insertAdjacentHTML('beforeend',`<div class='${classOf === 'alphabet' ? 'letter' : 'secret'}'>${classOf === 'alphabet' ? letter : ''}</div>`);
+        divTarget.insertAdjacentHTML('beforeend',`<div class='${classOf === 'alphabet' ? 'letter' : 'secret'}'>${classOf === 'alphabet' || clicked === letter ? letter : ''}</div>`);
     });
 }
+
+function checkClickedInSecret (clicked){
+    if (secretword.includes(clicked)) {
+        displayByLetter(secretword, 'secretWord', clicked);
+    } 
+} 
 
 const loadEvent = () => {
 
@@ -22,16 +30,20 @@ const loadEvent = () => {
         displayByLetter(abc, 'alphabet');
 
         displayByLetter(secretword, 'secretWord');
+        
+        const alphabetDiv = document.querySelectorAll('.letter');
+    
+        alphabetDiv.forEach(letterdiv => {
 
-    });
+            letterdiv.addEventListener('click', event => {
+                checkClickedInSecret(event.target.textContent);
+                console.log(event.target.textContent);
+            });
 
-    const alphabetDiv = document.querySelectorAll('.letter');
-
-    alphabetDiv.forEach(letterdiv => {
-        letterdiv.addEventListener('click', event => {
-            console.log(event.target.textContent);
         });
+
     });
+
 }
 
 window.addEventListener('load',loadEvent);
